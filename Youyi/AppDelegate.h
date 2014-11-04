9c16b04911379942b5706e1b3ebb3970637cdf98
gordonXie <xieguocheng_2006@163.com>
2014-11-04 13:52:34 +0800

    Initial Commit

---
+++ Youyi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Youyi
+//
+//  Created by xieguocheng on 14-11-4.
+//  Copyright (c) 2014年 xieguocheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Youyi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Youyi
+//
+//  Created by xieguocheng on 14-11-4.
+//  Copyright (c) 2014年 xieguocheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
